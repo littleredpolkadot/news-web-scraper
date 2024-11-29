73d1ef4f3de0f211a3ead7fd01d3cc1a6a5aad56
littleredpolkadot <61950373+littleredpolkadot@users.noreply.github.com>
2024-11-28 23:22:54 -0500

    Update scrapePR.py

--- scrapePR.py
+++ scrapePR.py
@@ -9,6 +9,7 @@ import os
 import re
 import json
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
 
 # Set up Chrome options
 options = webdriver.ChromeOptions()
@@ -26,12 +27,18 @@ base_url = "https://www.prnewswire.com/news-releases/news-releases-list/"
 current_page_file = "current_page_count.txt"
 progress_file = "progress.json"
 
+month = "10"
+year = "2024"
+hour = "17"
+day = "01"
+
+
 # Load the current page count or start from the first page
 if os.path.exists(current_page_file):
     with open(current_page_file, 'r') as f:
         current_count = int(f.read().strip())
 else:
-    current_count = 1  # Start from the first page
+    current_count = 102  # Start from the first page
 
 # Load progress from the previous run, if it exists
 if os.path.exists(progress_file):
@@ -45,51 +52,92 @@ keywords = ['venture', 'VC', 'series', 'round', 'valuation', 'unicorn']
 keyword_pattern = re.compile(r'|'.join(keywords), re.IGNORECASE)
 
 def fetch_article_details(link):
-    """Fetch article details and save it if it matches the keyword criteria."""
+    """Fetch article details and save it if it matches the keyword criteria. Save failed links."""
     try:
         driver.get(link)
         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "mm-0")))
+
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         wrap = soup.find('div', id="mm-0")
-        page_wrap = wrap.find('div', class_="page-wrap")
-        main_content = page_wrap.find('main', id="main").find("article", class_="news-release inline-gallery-template")
+        if wrap:
+            page_wrap = wrap.find('div', class_="page-wrap")
+            if page_wrap:
+                main_content = page_wrap.find('main', id="main").find("article", class_="news-release inline-gallery-template")
+                if not main_content:
+                    main_content = page_wrap.find('main', id="main").find("article", class_="news-release static-gallery-template")
 
-        if not main_content:
-            main_content = page_wrap.find('main', id="main").find("article", class_="news-release static-gallery-template")
+                if main_content:
+                    if (current_count == 102):
+                        found_header_and_container = main_content.find('header', class_='container release-header')
+                        third_row = found_header_and_container.find_all('div', class_='row')[3]
+                        #print(f"FOUND THIRD ROW: {third_row}")
+                        date = third_row.find("div", class_="col-lg-8 col-md-8 col-sm-7 swaping-class-left").find("p", class_="mb-no").get_text()
+                        #print(f"ARTICLE DATE: {date}")
+                        dt = datetime.strptime(date.replace(" ET", ""), "%b %d, %Y, %H:%M")
 
-        if main_content:
-            for script in main_content(['script', 'style']):
-                script.decompose()
+                        dt_plus_one_hour = dt + timedelta(hours=1)
 
-            formatted_text = main_content.get_text(separator='\n', strip=True)
-            found_keywords = keyword_pattern.findall(formatted_text)
+                        month = dt_plus_one_hour.strftime("%m") 
+                        day = dt_plus_one_hour.strftime("%d")   
+                        year = dt_plus_one_hour.strftime("%Y")  
+                        hour = dt_plus_one_hour.strftime("%H") 
 
-            if len(found_keywords) >= 2:
-                title = link.split('/')[-1]  # Fallback title (can be adjusted)
-                safe_title = re.sub(r'[<>:"/\\|?*]', '_', title)  # Remove invalid characters
-                file_path = os.path.join(save_folder, f"{safe_title}.txt")
 
-                # Try saving the article
-                try:
-                    with open(file_path, "w", encoding="utf-8") as f:
-                        f.write(formatted_text)
-                    print(f"Saved article: {safe_title}\n")
-                    
-                    # Only add the link to processed_links if the article was successfully saved
-                    return link  # Return the link if the article was saved successfully
-                except Exception as e:
-                    print(f"Failed to save {safe_title}: {e}")
-                    return None  # Return None if saving the article failed
+                        #print(f"Month: {month}, Day: {day}, Year: {year}, Hour: {hour}")
+
+
+
+                    for script in main_content(['script', 'style']):
+                        script.decompose()
+
+                    formatted_text = main_content.get_text(separator='\n', strip=True)
+                    found_keywords = keyword_pattern.findall(formatted_text)
+
+                    if len(found_keywords) >= 2:
+                        title = link.split('/')[-1]  # Fallback title (can be adjusted)
+                        safe_title = re.sub(r'[<>:"/\\|?*]', '_', title)  # Remove invalid characters
+                        file_path = os.path.join(save_folder, f"{safe_title}.txt")
+
+                        # Try saving the article
+                        try:
+                            with open(file_path, "w", encoding="utf-8") as f:
+                                f.write(formatted_text)
+                            print(f"Saved article: {safe_title}")
+                            return link  # Return the link if the article was saved successfully
+                        except Exception as e:
+                            print(f"Failed to save {safe_title}: {e}")
+                            append_failed_link(link)
+                            return None
+                else:
+                    print(f"Main content not found for article at {link}. Skipping...")
+                    append_failed_link(link)
+                    return None
+            else:
+                print(f"Page wrapper not found for {link}. Skipping...")
+                append_failed_link(link)
+                return None
         else:
-            print(f"Main content not found for article at {link}. Skipping...")
-            return None  # Return None if content isn't found
+            print(f"Wrap element not found for {link}. Skipping...")
+            append_failed_link(link)
+            return None
     except Exception as e:
         print(f"Error fetching article from {link}: {e}")
-        return None  # Return None if an error occurred during fetching
+        append_failed_link(link)
+        return None
+
+def append_failed_link(link):
+    """Append failed link to the failed links file."""
+    with open('failed_links.txt', 'a') as f:
+        f.write(link + '\n')
 
 def scrape_page(current_count):
     """Scrape a single page of articles and return article links."""
-    paging_link = f"{base_url}?page={current_count}&pagesize=100&month=10&day=01&year=2024&hour=17"
+    if (current_count == 103):
+        current_count = 1
+        paging_link = f"{base_url}?page={current_count}&pagesize=100&month={month}&day={day}&year={year}&hour={hour}"
+
+    else:
+        paging_link = f"{base_url}?page={current_count}&pagesize=100&month={month}&day={day}&year={year}&hour={hour}"
     driver.get(paging_link)
     print(f"Visiting page: {paging_link}")
 
